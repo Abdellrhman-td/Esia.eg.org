e5fae238e26c0df91fab379cd1c0b725962a1be8
Abdellrahman <qrtmbdalrhmnadl@gmail.com>
2024-11-17 19:56:51 +0200

    add new features

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
+// Select all elements with the "nav-link" class
+const navLinks = Array.from(document.querySelectorAll(".nav-link"));
+console.log (typeof(navLinks))
+// Loop through all nav-link elements
 
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    centeredSlides: false,
-    slidesPerGroupSkip: 1,
-    grabCursor: true,
-    keyboard: {
-      enabled: true,
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-    },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+navLinks.forEach((link) => {
+  // Add a click event listener to each link
+  link.addEventListener("click", (event) => {
+    // Remove "highlight" class from all nav-links
+    navLinks.forEach((link) => link.classList.remove("highlight"));
+
+    // Add "highlight" class to the clicked element
+    event.classList.add("highlight");
   });
+});
+
+
 
